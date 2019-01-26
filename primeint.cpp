d05c1da52c7da278ed03dc397ff44e83521f44b9
AdamRajoBenson <42169883+AdamRajoBenson@users.noreply.github.com>
2019-01-26 19:06:24 +0530

    Create primeint.cpp

---
+++ primeint.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+void main() 
+{
+int n,a,b,i,c=0;
+cin>>a;
+cin>>b;
+for(n=a+1;n<b;n++)
+{
+c=0;
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+{
+c++;
+break;
+}
+}
+if(c==0)
+{
+cout<<n<<"\t"; 
+}
+}
+	getch();
+}
